bf1b09039a53fca7591e4124ec965b046377d77c
SannaZzzzz <SannaZzzzz@users.noreply.github.com>
2025-04-07 12:03:58 +0000

    deploy: c2b314da8b04cf393303b534d2e05792a93bb98e

---
+++ _next/static/CP0YMRnTrNtFuWsxN8MuT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/771-31d34ed5c862188e.js","static/chunks/pages/index-3c4de9f7b2d3e80f.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
