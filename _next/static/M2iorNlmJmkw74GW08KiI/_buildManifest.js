d15675abb7671f494eb8ae6c233a5b21b01838e6
SannaZzzzz <SannaZzzzz@users.noreply.github.com>
2025-04-07 10:02:45 +0000

    deploy: 7def7cd65a67fb8746693ae613451bee84d452ca

--- _next/static/M2iorNlmJmkw74GW08KiI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/771-31d34ed5c862188e.js","static/chunks/pages/index-a9b2825557ad370e.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
